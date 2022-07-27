67d5cba731be582efc1e211dfdf1559eef432065
Pieter B <pieterbeernink@hotmail.com>
2022-07-27 23:01:09 +0200

    create UI table to display students

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,27 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent implements OnInit {
+  students: Student[] = []; // UI model
 
-  constructor(private _studentService: StudentService) { }
+  // List columns for Angular materialize table
+  displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'dateOfBirth',
+    'email',
+    'mobile',
+    'gender'
+  ];
+
+  // Create datasource for Angular materialize table
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+
+  constructor(private _studentService: StudentService) {}
 
   ngOnInit(): void {
     // Fetch students
-    this._studentService.getStudent()
-      .subscribe(
-        (successRepsonse) => {          
-          console.log('Students fetched');
-          console.log(successRepsonse[0].firstName);
-          console.log(successRepsonse[0].lastName);
-        },
-        (errorResponse) => {          
-          console.log(errorResponse);
-        }
-      ); // subscribe as studentservice returns an observable
+    this._studentService.getStudent().subscribe(
+      (successRepsonse) => {
+        this.students = successRepsonse;
+        this.dataSource = new MatTableDataSource<Student>(this.students); // fill dataSource with list of students
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    ); // subscribe as studentservice returns an observable
   }
 }
